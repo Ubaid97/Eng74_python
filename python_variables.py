2dd854bc39cc857fa13abe318c98e35431e2712f
Ubaid-ur-rehman Muhammad <zdac091@live.rhul.ac.uk>
2020-11-02 15:05:54 +0000

    python variables - input and type

--- python_variables.py
+++ python_variables.py
@@ -1,3 +1,15 @@
 # How to create a variable
-name = 'Ubaid'
+# name = 'Ubaid' # String variable
+age = 22 # Integer
+travel_allowance = 2.4 # Float
+# Type of variables: string, int, float, and boolean
+
+# How to take user input
+name = input("Please enter your name: ")
+
 print(name)
+print(age)
+print(travel_allowance)
+
+# How to find out type of variable
+print(type(age))
