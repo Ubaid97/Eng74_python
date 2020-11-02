b5e383ecd6db13e428038980187f18a91d8b930b
Ubaid-ur-rehman Muhammad <zdac091@live.rhul.ac.uk>
2020-11-02 15:40:16 +0000

    exercise 1

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ## Variables
 - a variable is a place holder to store data
+
+## Exercises
+### Exercise 1
+- Exercise 1 - get and display the following information from a user: Name, DOB, and Age, as well as the data type for each variable 
---
+++ Variable_exercise.py
@@ -0,0 +1,9 @@
+# Exercise 1 - get and display tye following information from a user: Name, DOB, and Age
+
+name = input("Please enter your name: ")
+DOB = input("please enter your date of birth in the format dd/mm/yyyy: ")
+age = input("please enter your age: ")
+
+print(name, type(name))
+print(DOB, type(DOB))
+print(age, type(age))
