75b0193dd12e4645002c96b44aa83537a9f16a82
Ubaid-ur-rehman Muhammad <zdac091@live.rhul.ac.uk>
2020-11-02 16:55:09 +0000

    string slicing

---
+++ string_slicing.py
@@ -0,0 +1,13 @@
+# String slicing - indexing starts from 0
+
+greetings = 'Hello World!'
+# to print out only 'Hello'
+print(greetings[0:5])
+
+# .strip() - deletes all spaces at the end of string
+# text.count(string) - returns the number of times string occurs in text
+# text.replace(string1, string2) - replaces instances of string1 with string2 in text
+# string.capitalize() - capitalises first character of string
+# .upper() / .lower() - changes all characters to upper/lower case
+
+print(f"{greetings}, nice to meet you!")
