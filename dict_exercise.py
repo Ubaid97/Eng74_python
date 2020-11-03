d29a87fd9fdbc78fd2f24e2a70f2e6934dc9f806
Ubaid-ur-rehman Muhammad <zdac091@live.rhul.ac.uk>
2020-11-03 14:37:14 +0000

    dict exercise

---
+++ dict_exercise.py
@@ -0,0 +1,28 @@
+# Creating dictionary containing user details
+user_details = {"first name": "John",
+                "last name": "Doe",
+                "Date of birth": "03/12/1997",
+                "age": "22",
+                "address": "12 Sesame st",
+                "course": "DevOps",
+                "grades": "A",
+                "hobbies": ["football", "movies", "reading"]
+}
+
+print(user_details)
+print(type(user_details))
+
+# Added postcode to the dictionary
+user_details["postcode"] = "ab1 2cd"
+print(user_details)
+
+# Removed grades
+user_details.pop("grades")
+print(user_details)
+
+# Changed house number from 12 to 13 in the address
+user_details["address"] = "13 Sesame st"
+print(user_details)
+
+# displayed hobbies list in reverese order
+print(user_details["hobbies"][::-1])
