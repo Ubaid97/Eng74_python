53dd78c3c60e86f3f8528fa850209c2d991bb22d
Ubaid-ur-rehman Muhammad <zdac091@live.rhul.ac.uk>
2020-11-03 14:59:59 +0000

    sets

---
+++ sets.py
@@ -0,0 +1,10 @@
+# Sets don't have indices - they're unordered
+
+car_parts = {"wheels", "doors", "radio", }
+print(car_parts)
+
+car_parts.add("seatbelt")
+print(car_parts)
+
+car_parts.remove("wheels")
+print(car_parts)
